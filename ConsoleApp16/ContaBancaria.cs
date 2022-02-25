89e73c2acca3f72c74bbce542933671de571550c
Samuel Ribeiro <samuca3004@hotmail.com>
2022-02-25 17:40:00 -0300

    Add project files.

---
+++ ConsoleApp16/ContaBancaria.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ConsoleApp16
+{
+    internal class ContaBancaria
+    {
+        public string Nome { get; set; }
+        public int NumeroConta { get; private set; }
+        public double Saldo { get; set; }
+
+        public ContaBancaria(string nome, int numeroConta)
+        {
+            Nome = nome;
+            NumeroConta = numeroConta;
+
+        }
+
+        public ContaBancaria(string nome, int numeroConta, double saldo) : this(nome, numeroConta)
+        {
+            Nome = Nome;
+            NumeroConta = numeroConta;
+            Saldo = saldo;
+            
+
+        }
+
+        public double Depositar(double saldo)
+        {
+           return Saldo += saldo;
+        }
+
+        public double Sacar(double saldo)
+        {
+           return Saldo -= saldo;
+        }
+
+        public override string ToString()
+        {
+            return "Titular: " + Nome + " Numero da conta: " + NumeroConta + " Seu saldo é de: " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria NovaConta;
+            Console.Write("Entre com nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Entre numero da conta: ");
+            int nConta = int.Parse(Console.ReadLine());
+            Console.Write("Deseja fazer um deposito inicial: S/N ");
+            char resp = char.Parse(Console.ReadLine());
+
+            if(resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre com valor para deposito: ");
+                double dep = double.Parse(Console.ReadLine());
+                
+                NovaConta = new ContaBancaria(nome, nConta);
+                double sal = NovaConta.Depositar(dep);
+                Console.WriteLine();
+                Console.WriteLine(NovaConta);
+                Console.WriteLine();
+            }
+            else
+            {
+                NovaConta = new ContaBancaria(nome, nConta);
+                Console.WriteLine(NovaConta);
+            }
+
+
+            char d = default;
+            do
+            {
+                
+                Console.Write("Deseja fazer um novo deposito: S/N ");
+                d = char.Parse(Console.ReadLine());
+                if (d == 's' || d == 'S')
+                {
+                    Console.Write("Qual Valor para deposito: ");
+                    double dep = double.Parse(Console.ReadLine());
+                    NovaConta.Depositar(dep);
+                    Console.WriteLine();
+                    Console.WriteLine(NovaConta);
+                    Console.WriteLine();
+                }
+                
+
+            } while(d == 'S' || d == 's');
+
+            do
+            {
+                Console.WriteLine();
+                Console.Write("Deseja fazer um saque: S/N ");
+                d = char.Parse(Console.ReadLine());
+                if (d == 's' || d == 'S')
+                {
+                    
+                    Console.Write("Digite o valor do saque: ");
+                    double dep = double.Parse(Console.ReadLine());
+                    NovaConta.Sacar(dep);
+                    Console.WriteLine();
+                    Console.WriteLine(NovaConta);
+                    Console.WriteLine();
+                }
+
+
+            } while (d == 'S' || d == 's');
+
+            Console.WriteLine();
+            Console.WriteLine(NovaConta);
+            Console.WriteLine();
+
+            Console.WriteLine("Obrigado por Utilizar Samuel Banks");
+
+
+        }
+    }
+}
